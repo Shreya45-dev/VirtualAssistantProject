b755bb9e92426cff94ad2cf36395ddc8d0736de0
Shreya Pandey <shreyap2276@gmail.com>
2026-07-13 02:02:31 +0530

    in

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 
 function connectDB(){
-    mongoose.connect(MONGODB_URL)
+    mongoose.connect(process.env.MONGODB_URL)
     .then(()=>{
         console.log("Mongodb connected")
     })
